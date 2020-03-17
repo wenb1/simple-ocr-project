cca288ecacbfdff492979e020750d4026d8e7e90
wenb1 <bowen960721@gmail.com>
2020-03-16 22:12:39 -0400

    first commit

--- ocr_letters/models.py
+++ ocr_letters/models.py
@@ -2,7 +2,6 @@ from django.db import models
 
 # Create your models here.
 from django.db import models
-from django.utils import timezone
 
 
 class mysqlImage(models.Model):
@@ -11,7 +10,3 @@ class mysqlImage(models.Model):
     letters = models.CharField(max_length=100)
     isdelete = models.IntegerField(default=0)
     addtime = models.DateTimeField(auto_now_add=True)
-    modifytime = models.DateTimeField(auto_now=True)
-
-    class Meta:
-        db_table = 'tb_ocr_letters'  # 数据表名称
